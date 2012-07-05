f7091bb230e7eabe7edd50ad6dca89794e847a6b
Iuri Diniz <iuridiniz@gmail.com>
2012-07-04 21:58:45 -0300

    Support a subscribe to create on pubsub

--- tests/test_thoonk_client.py
+++ tests/test_thoonk_client.py
@@ -7,6 +7,7 @@ from twisted.trial import unittest
 from twisted.internet import defer
 from twisted.internet import reactor
 from twisted.internet.endpoints import TCP4ClientEndpoint
+from txthoonk.client import FeedExists
 
 REDIS_HOST = "localhost"
 REDIS_PORT = 6381
@@ -14,8 +15,21 @@ REDIS_DB = 1
 
 class Test(unittest.TestCase):
 
+    def check_called(self, func):
+        self.called[func] = False
+        def _check(*args, **kwargs):
+            self.called[func] = True
+            return func(*args, **kwargs)
+        return _check
+
+    def final_check(self):
+        for func in self.called.keys():
+            self.assertTrue(self.called[func], "%s was not called" % (func,))
+
     @defer.inlineCallbacks
     def setUp(self):
+        self.called = dict()
+
         from txthoonk.client import ThoonkFactory, Thoonk, ThoonkPubSub, \
             ThoonkPubSubFactory, Redis
         self.thoonk = Thoonk(Redis()) # pydev: force code completion
@@ -38,12 +52,28 @@ class Test(unittest.TestCase):
         endpoint = TCP4ClientEndpoint(reactor, REDIS_HOST, REDIS_PORT)
         self.pubsub = yield endpoint.connect(ThoonkPubSubFactory())
 
+        self._configure_wrappers()
+
+    def _configure_wrappers(self):
+        msgRcvOrig = self.pubsub.redis.messageReceived
+        self.msg_rcv = defer.Deferred()
+        def msgRcvWrp(*args, **kwargs):
+            msgRcvOrig(*args, **kwargs)
+            self.msg_rcv.callback(None)
+            self.msg_rcv = defer.Deferred()
+        self.pubsub.redis.messageReceived = msgRcvWrp
 
     def tearDown(self):
-        def closeRedis():
+        def closeRedis(*args):
             self.thoonk.redis.transport.loseConnection()
             self.pubsub.redis.transport.loseConnection()
-        reactor.callLater(0, closeRedis) #@UndefinedVariable
+            self.final_check()
+        #d = defer.Deferred()
+        #d.addCallback(closeRedis)
+        #reactor.callLater(0.1, d.callback, 0) #@UndefinedVariable        
+        #return d
+
+        return reactor.callLater(0, closeRedis) #@UndefinedVariable
 
     @defer.inlineCallbacks
     def testPing(self):
@@ -53,9 +83,59 @@ class Test(unittest.TestCase):
     @defer.inlineCallbacks
     def testCreateFeed(self):
         feed_name = 'test_feed'
+
         yield self.thoonk.create_feed(feed_name)
         ret = yield self.thoonk.redis.smembers("feeds")
         self.assertEqual(set([feed_name]), ret)
+
+    @defer.inlineCallbacks
+    def testHandlerCreateFeed(self):
+        feed1 = 'feed1'
+
+        @self.check_called
+        def onCreate(ret_name):
+            self.assertEqual(ret_name, feed1)
+
+        yield self.pubsub.register_handler('create', onCreate)
+
+        # Assuring that redis.messageReceived (pubsub was called)
+        cb = self.msg_rcv
+        yield self.thoonk.create_feed(feed1)
+        yield cb
+
+
+    @defer.inlineCallbacks
+    def testHandlerCreateRemove(self):
+        feed1 = 'feed1'
+        feed2 = 'feed2'
+
+        @self.check_called
+        def onCreate(ret_name):
+            self.assertEqual(ret_name, feed1)
+        id_ = yield self.pubsub.register_handler('create', onCreate)
+
+        # Assuring that redis.messageReceived (pubsub was called)
+        cb = self.msg_rcv
+        yield self.thoonk.create_feed(feed1)
+        yield cb
+
+        ret = yield self.thoonk.redis.smembers("feeds")
+        self.assertEqual(set([feed1]), ret)
+
+        # same feed, must return a error
+
+        self.assertFailure(self.thoonk.create_feed(feed1), FeedExists)
+
+        # removing
+        self.pubsub.remove_handler(id_)
+
+        # Assuring that redis.messageReceived (pubsub was called)
+        cb = self.msg_rcv
+        yield self.thoonk.create_feed(feed2)
+        yield cb
+
+        ret = yield self.thoonk.redis.smembers("feeds")
+        self.assertEqual(set([feed2, feed1]), ret)
 
 
 if __name__ == "__main__":
--- txthoonk/client.py
+++ txthoonk/client.py
@@ -2,14 +2,33 @@ from zope.interface import implements #@UnresolvedImport
 
 from twisted.internet.protocol import ReconnectingClientFactory
 
-from txredis.protocol import Redis
+from txredis.protocol import Redis, RedisSubscriber, defer
 from twisted.internet import interfaces
 
+import uuid
+import itertools
+from twisted.python import failure
+
+
+class FeedExists(Exception):
+    pass
+
+class FeedDoesNotExist(Exception):
+    pass
+
+class Empty(Exception):
+    pass
+
+class NotListening(Exception):
+    pass
+
+
 class ThoonkBase(object):
+    SEPARATOR = "\x00"
     implements(interfaces.IProtocol)
-    redis = Redis() # pydev: force code completion
     def __init__(self, redis):
         self.redis = redis
+        self._uuid = uuid.uuid4().hex
 
     def dataReceived(self, data):
         """
@@ -61,10 +80,18 @@ class ThoonkBase(object):
         self.redis.connectionMade()
 
 class Thoonk(ThoonkBase):
-    def create_feed(self, feed_name):
-        d = self.redis.sadd("feeds", feed_name)
-        return d
+    redis = Redis() # pydev: force code completion
 
+    def create_feed(self, feed_name):
+        def _publish(ret):
+            if ret == 1:
+                return self.redis.publish("newfeed", self.SEPARATOR.join([feed_name, self._uuid]))
+            else:
+                d = defer.Deferred()
+                d.errback(FeedExists())
+                return d
+
+        return self.redis.sadd("feeds", feed_name).addCallback(_publish)
 
 class ThoonkFactory(ReconnectingClientFactory):
     protocol = Redis
@@ -93,9 +120,95 @@ class ThoonkFactory(ReconnectingClientFactory):
         return self.protocol_wrapper(redis)
 
 class ThoonkPubSub(ThoonkBase):
-    pass
+    redis = RedisSubscriber() # pydev: force code completion
+    def __init__(self, redis):
+        redis.messageReceived = self.messageReceived
+        redis.channelSubscribed = self.channelSubscribed
+
+        self._handlers = {'id_gen': itertools.count(), #@UndefinedVariable
+                          'evt_handlers': {'create': dict()},
+                          'id2evt' : {}}
+
+        self._channels_maps = {"newfeed": "create", "onrock": None}
+        # delay subscribe 
+        self._subscribed = {'running': False,
+                            'subscribed': False,
+                            'defers': {},
+                            'dl': None}
+
+        super(ThoonkPubSub, self).__init__(redis)
+
+    def _subscribe(self):
+        if self._subscribed['subscribed']:
+            return defer.Deferred.callback(None)
+
+        if self._subscribed['running']:
+            return self._subscribed['dl']
+
+        self._subscribed['running'] = True
+        def set_subscribed(*args):
+            self._subscribed['running'] = False
+            self._subscribed['subscribed'] = True
+            return None
+
+        for channel in self._channels_maps.keys():
+            self.redis.subscribe(channel)
+            d = defer.Deferred()
+            self._subscribed['defers'][channel] = d
+
+        dl = defer.DeferredList(self._subscribed['defers'].values())
+
+        return dl.addCallback(set_subscribed)
+
+    def register_handler(self, evt, handler):
+        def register_callback(*args):
+            id_ = self._handlers['id_gen'].next()
+
+            # store map id -> evt
+            self._handlers['id2evt'][id_] = evt
+
+            # store handler on evt
+            self._handlers['evt_handlers'][evt][id_] = handler
+            return id_
+
+        if evt not in self._handlers['evt_handlers'].keys():
+            d = defer.Deferred()
+            d.callback(None)
+
+        return self._subscribe().addCallback(register_callback)
+
+
+    def remove_handler(self, id_):
+        evt = self._handlers['id2evt'].get(id_)
+        if not evt:
+            return
+
+        del self._handlers['evt_handlers'][evt][id_]
+        del self._handlers['id2evt'][id_]
+
+    def messageReceived(self, channel, message):
+        """
+        Called when this connection is subscribed to a channel that
+        has received a message published on it.
+        """
+        evt = self._channels_maps.get(channel)
+        if evt is None:
+            return
+
+        if not self.SEPARATOR in message:
+            return
+
+        for handler in self._handlers['evt_handlers'][evt].values():
+            handler(message.split(self.SEPARATOR)[0])
+
+    def channelSubscribed(self, channel, numSubscriptions):
+        """
+        Called when a channel is subscribed to.
+        """
+        d = self._subscribed['defers'][channel]
+        d.callback(None)
 
 class ThoonkPubSubFactory(ThoonkFactory):
-    protocol = Redis
+    protocol = RedisSubscriber
     protocol_wrapper = ThoonkPubSub
 
