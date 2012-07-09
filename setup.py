579c240937e65d2b58e90902a2cad9347d628dd6
Iuri Diniz <iuridiniz@gmail.com>
2012-07-09 11:40:44 -0300

    Update setup.py
    - using description from docstring of txthoonk
    - update pre-requisites
    - configuring setup.py test
    - remove tests from packages

--- setup.py
+++ setup.py
@@ -3,27 +3,35 @@
 import os
 from setuptools import setup
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+#def read(fname):
+#    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 def get_version():
     from txthoonk import __version__ as v
     return v
 
+def get_long_description():
+    import txthoonk as t
+    # remove blank lines
+    return "\n".join([l for l in t.__doc__.splitlines() if l]) #@UndefinedVariable
+
 sdict = {
     'name' : 'txthoonk',
     'version' : get_version(),
-    'packages' : ['txthoonk', 'tests'],
+    'packages' : ['txthoonk'],
     'description' : 'Python/Twisted client for Thoonk',
     'author' : 'Iuri Diniz',
     'author_email' : 'iuridiniz@gmail.com',
     'maintainer' : 'Iuri Diniz',
     'maintainer_email' : 'iuridiniz@gmail.com',
     'license': 'MIT',
-    'url': 'https://github.com/iuridiniz/txthoonk',
+    'url': 'https://github.com/Diginet/txthoonk',
     'download_url':'http://pypi.python.org/pypi/txthoonk',
     'keywords': ['Redis', 'Thoonk', 'Twisted'],
-    'long_description': read('README.rst'),
+    'long_description': get_long_description(),
+    'install_requires': ['Twisted>=10.1.0',
+                         'txredis>=2.2'],
+    'test_suite': 'tests',
     'classifiers' : [
         'Development Status :: 1 - Planning',
         'Environment :: Console',
--- txthoonk/__init__.py
+++ txthoonk/__init__.py
@@ -1,3 +1,7 @@
 #!/usr/bin/env python
+"""
+txThoonk is a Thoonk client library for Twisted Python.
 
+Read more about Thoonk at http://blog.thoonk.com
+"""
 __version__ = "0.0.1"
